eee4f8b1b1bb70c37d9b31a0a2956d5cbfa1d88b
Leandroyyy <leandro.cavallari123@gmail.com>
2023-07-10 11:50:56 -0300

    feat: realized comments use case and repositories

---
+++ src/domain/forum/application/use-cases/comment-on-answer.spec.ts
@@ -0,0 +1,35 @@
+import { InMemoryAnswerRepository } from "test/repositories/in-memory-answers-repository";
+import { makeAnswer } from "test/factories/make-answer";
+import { CommentOnAnswerUseCase } from "./comment-on-answer";
+import { InMemoryAnswerCommentRepository } from "test/repositories/in-memory-answer-comments-repository";
+
+let inMemoryAnswerRepository: InMemoryAnswerRepository;
+let inMemoryAnswerCommentsRepository: InMemoryAnswerCommentRepository;
+let sut: CommentOnAnswerUseCase;
+
+describe("Choose Answer Best Answer", () => {
+  beforeEach(() => {
+    inMemoryAnswerRepository = new InMemoryAnswerRepository();
+    inMemoryAnswerCommentsRepository = new InMemoryAnswerCommentRepository();
+    sut = new CommentOnAnswerUseCase(
+      inMemoryAnswerRepository,
+      inMemoryAnswerCommentsRepository
+    );
+  });
+
+  it("should be able to comment on answer", async () => {
+    const answer = makeAnswer({
+      content: 'Comentario teste'
+    })
+
+    await inMemoryAnswerRepository.create(answer)
+
+    await sut.execute({
+      answerId: answer.id.toString(),
+      authorId: answer.authorId.toString(),
+      content: answer.content
+    });
+
+    expect(inMemoryAnswerRepository.items[0].content).toEqual('Comentario teste');
+  });
+});
---
+++ src/domain/forum/application/use-cases/comment-on-question.spec.ts
@@ -0,0 +1,35 @@
+import { InMemoryQuestionRepository } from "test/repositories/in-memory-questions-repository";
+import { makeQuestion } from "test/factories/make-question";
+import { InMemoryQuestionCommentRepository } from "test/repositories/in-memory-question-comments-repository";
+import { CommentOnQuestionUseCase } from "./comment-on-question";
+
+let inMemoryQuestionRepository: InMemoryQuestionRepository;
+let inMemoryQuestionCommentsRepository: InMemoryQuestionCommentRepository;
+let sut: CommentOnQuestionUseCase;
+
+describe("Choose Question Best Answer", () => {
+  beforeEach(() => {
+    inMemoryQuestionRepository = new InMemoryQuestionRepository();
+    inMemoryQuestionCommentsRepository = new InMemoryQuestionCommentRepository();
+    sut = new CommentOnQuestionUseCase(
+      inMemoryQuestionRepository,
+      inMemoryQuestionCommentsRepository
+    );
+  });
+
+  it("should be able to comment on question", async () => {
+    const question = makeQuestion({
+      content: 'Comentario teste'
+    })
+
+    await inMemoryQuestionRepository.create(question)
+
+    await sut.execute({
+      questionId: question.id.toString(),
+      authorId: question.authorId.toString(),
+      content: question.content
+    });
+
+    expect(inMemoryQuestionRepository.items[0].content).toEqual('Comentario teste');
+  });
+});
